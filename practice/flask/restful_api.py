aececd1dbb79c3c623dbe7950c62883d30483e77
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-10-11 19:40:40 -0700

    started learning authentication

--- practice/flask/restful_api.py
+++ practice/flask/restful_api.py
@@ -2,21 +2,24 @@ from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'wayne' #needs to be long and complicated and hidden
 api = Api(app)
 
 items = []
 
 class Item(Resource):
   def get(self, name):
-    for item in items: 
-      if item['name'] == name:
-        return item
-    return {'item': None}, 404
+    item = next(filter(lambda x: x['name'] == name, items), None)
+    return {'item': None}, 200 if item else 404
   
   def post(self, name):
-    item = {'name': name, 'price': 12.00}
+    if next(filter(lambda x: x['name'] == name, items), None):
+      return {'message': "An item with name '{}' already exists.".format(name)}, 400
+
+    data = request.get_json()
+    item = {'name': name, 'price': data['price']}
     items.append(item)
-    return item, 20
+    return item, 201
 
 class ItemList(Resource):
   def get(self):
