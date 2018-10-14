eccceed9cd58ae705d0e1ee8794db3ce19f7ab57
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-10-14 13:41:32 -0700

    added JWT authentication

--- practice/flask/restful_api.py
+++ practice/flask/restful_api.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
 app.secret_key = 'wayne' #needs to be long and complicated and hidden
 api = Api(app)
 
+jwt = JWT(app, authenticate, identity) # /auth
+
 items = []
 
 class Item(Resource):
+  @jwt_required()
   def get(self, name):
     item = next(filter(lambda x: x['name'] == name, items), None)
     return {'item': None}, 200 if item else 404
